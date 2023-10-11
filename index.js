7dd3a78d663521dc6863e47fad0bf4b795dc57be
Eckra Wangui <+>
2023-10-11 15:26:41 +0300

    new sytles

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const notes = document.getElementById("notes");
-    const noteText = document.getElementById("note-text");
-    const addButton = document.getElementById("add-button");
-    const saveButton = document.getElementById("save-button");
-    const deleteButton = document.getElementById("delete-button");
+const noteInput = document.getElementById("note-input");
+const addBtn = document.getElementById("add-button");
+const notesList = document.getElementById("notes-list");
 
-    addButton.addEventListener("click", function() {
-        const text = noteText.value;
-        if (text.trim() !== "") {
-            const listItem = document.createElement("li");
-            listItem.innerText = text;
-            notes.appendChild(listItem);
-            noteText.value = "";
-        }
-    });
+addBtn.addEventListener("click", addNote);
 
-    saveButton.addEventListener("click", function() {
-        const text = noteText.value;
-        if (text.trim() !== "") {
-            const listItem = notes.querySelector("li.selected");
-            if (listItem) {
-                listItem.innerText = text;
-            }
-        }
-    });
+function addNote() {
+    const noteText = noteInput.value.trim();
 
-    deleteButton.addEventListener("click", function() {
-        const listItem = notes.querySelector("li.selected");
-        if (listItem) {
-            notes.removeChild(listItem);
-            noteText.value = "";
-        }
-    });
+    if (noteText === "") return;
 
-    notes.addEventListener("click", function(event) {
-        if (event.target.tagName === "LI") {
-            const listItem = event.target;
-            noteText.value = listItem.innerText;
-            selectListItem(listItem);
-        }
-    });
+    const noteItem = document.createElement("div");
+    noteItem.className = "note";
 
-    function selectListItem(listItem) {
-        const items = notes.getElementsByTagName("li");
-        for (const item of items) {
-            item.classList.remove("selected");
-        }
-        listItem.classList.add("selected");
-    }
-});
+    noteItem.innerHTML = `
+        <p>${noteText}</p>
+        <button class="delete" onclick="deleteNote(this)">Delete</button>
+    `;
+
+    notesList.appendChild(noteItem);
+    noteInput.value = "";
+}
+
+function deleteNote(button) {
+    const noteItem = button.parentElement;
+    notesList.removeChild(noteItem);
+}
