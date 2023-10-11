6e51d8f59872142d2265bd9b4bc8d32123d5e7f6
Eckra Wangui <+>
2023-10-11 15:49:29 +0300

    updated app structure

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const noteTitleInput = document.getElementById("note-title");
 const noteInput = document.getElementById("note-input");
 const addBtn = document.getElementById("add-button");
 const notesList = document.getElementById("notes-list");
@@ -5,20 +6,56 @@ const notesList = document.getElementById("notes-list");
 addBtn.addEventListener("click", addNote);
 
 function addNote() {
+    const noteTitle = noteTitleInput.value.trim();
     const noteText = noteInput.value.trim();
 
-    if (noteText === "") return;
+    if (noteTitle === "" || noteText === "") return;
 
     const noteItem = document.createElement("div");
     noteItem.className = "note";
 
+    const date = new Date();
+    const formattedDate = date.toLocaleString();
+
     noteItem.innerHTML = `
-        <p>${noteText}</p>
+        <h3 class="note-title">${noteTitle}</h3>
+        <p class="note-text">${noteText}</p>
+        <p class="note-date">${formattedDate}</p>
+        <button class="edit" onclick="editNote(this)">Edit</button>
         <button class="delete" onclick="deleteNote(this)">Delete</button>
+        <button class="save" onclick="saveNote(this)">Save</button>
     `;
 
     notesList.appendChild(noteItem);
+
+    noteTitleInput.value = "";
     noteInput.value = "";
+}
+
+function editNote(button) {
+    const noteItem = button.parentElement;
+    const noteText = noteItem.querySelector(".note-text");
+    const noteTitle = noteItem.querySelector(".note-title");
+    const saveButton = noteItem.querySelector(".save");
+    
+    noteText.contentEditable = true;
+    noteText.focus();
+    noteTitle.contentEditable = true;
+    noteTitle.focus();
+    button.style.display = "none";
+    saveButton.style.display = "inline";
+}
+
+function saveNote(button) {
+    const noteItem = button.parentElement;
+    const noteText = noteItem.querySelector(".note-text");
+    const noteTitle = noteItem.querySelector(".note-title");
+    const editButton = noteItem.querySelector(".edit");
+    
+    noteText.contentEditable = false;
+    noteTitle.contentEditable = false;
+    button.style.display = "none";
+    editButton.style.display = "inline";
 }
 
 function deleteNote(button) {
