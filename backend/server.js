f33d4496132580e08298e947adb7f143dca1dde4
Eckra Wangui <+>
2023-10-12 22:49:05 +0300

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
+server.use('/', router)
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
